aabd96702a024e4b7a2f483a8bdf0539ab8bc3f9
Christophe Lambin <christophe.lambin@gmail.com>
2024-04-13 14:30:31 +0200

    auth handler

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -283,12 +283,13 @@ func Test_loggedRequest(t *testing.T) {
 	r := makeHTTPRequest(http.MethodGet, "example.com", "/foo/bar")
 	r.AddCookie(&http.Cookie{Name: sessionCookieName, Value: "foo"})
 	r.AddCookie(&http.Cookie{Name: oauthStateCookieName, Value: "bar"})
+	r.RemoteAddr = "127.0.0.1:0"
 
 	var out bytes.Buffer
 	l := testutils.NewJSONLogger(&out, slog.LevelInfo)
 	l.Info("request", "r", loggedRequest{r: r})
 
-	want := `{"level":"INFO","msg":"request","r":{"http":"https://traefik/","traefik":"https://example.com/foo/bar","cookies":"_simple_auth,state","user-agent":"unit-test"}}
+	want := `{"level":"INFO","msg":"request","r":{"http":"https://traefik/","traefik":"https://example.com/foo/bar","cookies":"_simple_auth,state","source":"127.0.0.1:0"}}
 `
 	if got := out.String(); got != want {
 		t.Errorf("got %q, want %q string", got, want)
