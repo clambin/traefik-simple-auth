062db5815351c8bba84f220e42aa10d4d95122b5
Christophe Lambin <christophe.lambin@gmail.com>
2024-07-20 13:45:55 +0200

    refactor: simplify server mgmt

--- internal/cmd/run.go
+++ internal/cmd/run.go
@@ -11,6 +11,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/redis/go-redis/v9"
+	"golang.org/x/sync/errgroup"
 	"log/slog"
 	"net/http"
 	"time"
@@ -19,53 +20,36 @@ import (
 func Run(ctx context.Context, cfg configuration.Configuration, registry prometheus.Registerer, version string, logger *slog.Logger) error {
 	logger.Info("traefik-simple-auth starting", "version", version)
 
-	promErr := make(chan error)
-	go func() {
-		m := http.NewServeMux()
-		m.Handle("/metrics", promhttp.Handler())
-		promErr <- runHTTPServer(ctx, cfg.PromAddr, m)
-	}()
-
 	metrics := server.NewMetrics("traefik_simple_auth", "", prometheus.Labels{"provider": cfg.Provider})
 	registry.MustRegister(metrics)
 	sessionStore := sessions.New(cfg.SessionCookieName, cfg.Secret, cfg.TTL)
 	stateStore := makeStateStore(cfg.CacheConfiguration)
 	s := server.New(ctx, sessionStore, stateStore, cfg, metrics, logger)
 
-	serverErr := make(chan error)
-	go func() {
-		serverErr <- runHTTPServer(ctx, cfg.Addr, s)
-	}()
+	var g errgroup.Group
+	runServer(ctx, &g, &http.Server{Addr: cfg.PromAddr, Handler: promhttp.Handler()})
+	runServer(ctx, &g, &http.Server{Addr: cfg.Addr, Handler: s})
 
-	err := errors.Join(<-serverErr, <-promErr)
 	logger.Info("traefik-simple-auth stopped")
-	return err
+	return g.Wait()
 }
 
-func runHTTPServer(ctx context.Context, addr string, handler http.Handler) error {
-	httpServer := &http.Server{Addr: addr, Handler: handler}
-	errCh := make(chan error)
-	go func() {
-		err := httpServer.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			err = nil
+func runServer(ctx context.Context, g *errgroup.Group, s *http.Server) {
+	subCtx, cancel := context.WithCancel(ctx)
+	g.Go(func() error {
+		defer cancel()
+		if err := s.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			return err
 		}
-		errCh <- err
-	}()
-
-	select {
-	case err := <-errCh:
-		return err
-	case <-ctx.Done():
-	}
-
-	ctx2, cancel2 := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel2()
-	err := httpServer.Shutdown(ctx2)
-	if errors.Is(err, http.ErrServerClosed) {
-		err = nil
-	}
-	return err
+		return nil
+	})
+	g.Go(func() error {
+		<-subCtx.Done()
+		if err := s.Shutdown(context.Background()); !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+		return nil
+	})
 }
 
 func makeStateStore(cfg configuration.CacheConfiguration) state.States {
@@ -93,6 +77,6 @@ func makeStateStore(cfg configuration.CacheConfiguration) state.States {
 	return state.States{
 		Cache:     backend,
 		Namespace: "github.com/clambin/traefik-simple-auth",
-		TTL:       10 * time.Minute,
+		TTL:       5 * time.Minute,
 	}
 }
