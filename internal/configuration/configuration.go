f4b9c1dd6a849a62f1c41d73a09746f12f3d6d2f
Christophe Lambin <christophe.lambin@gmail.com>
2025-01-18 15:13:15 +0100

    refactor(config): simpler config parsing

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -14,28 +14,6 @@ import (
 	"time"
 )
 
-var (
-	debug              = flag.Bool("debug", false, "Log debug messages")
-	addr               = flag.String("addr", ":8080", "The address to listen on for HTTP requests")
-	promAddr           = flag.String("prom", ":9090", "The address to listen on for Prometheus scrape requests")
-	sessionCookieName  = flag.String("session-cookie-name", "_traefik_simple_auth", "The cookie name to use for authentication")
-	sessionExpiration  = flag.Duration("expiry", 30*24*time.Hour, "How long a session remains valid")
-	authPrefix         = flag.String("auth-prefix", "auth", "Prefix to construct the authRedirect URL from the domain")
-	domainString       = flag.String("domain", "", "Domain to allow access")
-	users              = flag.String("users", "", "Comma-separated list of usernames to allow access")
-	provider           = flag.String("provider", "google", "OAuth2 provider")
-	oidcIssuerURL      = flag.String("provider-oidc-issuer", "https://accounts.google.com", "The OIDC Issuer URL to use (only used when provider is oidc")
-	clientId           = flag.String("client-id", "", "OAuth2 Client ID")
-	clientSecret       = flag.String("client-secret", "", "OAuth2 Client Secret")
-	secret             = flag.String("secret", "", "Secret to use for authentication (base64 encoded)")
-	cacheBackend       = flag.String("cache", "memory", "The backend to use for caching CSFR states. memory, memcached and redis are supported")
-	cacheMemcachedAddr = flag.String("cache-memcached-addr", "", "memcached address to use (only used when cache backend is memcached)")
-	cacheRedisAddr     = flag.String("cache-redis-addr", "", "redis address to use (only used when cache backend is redis)")
-	cacheRedisDatabase = flag.Int("cache-redis-database", 0, "redis database to use (only used when cache backend is redis)")
-	cacheRedisUsername = flag.String("cache-redis-username", "", "Redis username (only used when cache backend is redis)")
-	cacheRedisPassword = flag.String("cache-redis-password", "", "Redis password (only used when cache backend is redis)")
-)
-
 type Configuration struct {
 	Whitelist          whitelist.Whitelist
 	Addr               string
@@ -54,45 +32,42 @@ type Configuration struct {
 }
 
 func GetConfiguration() (Configuration, error) {
-	flag.Parse()
 	cfg := Configuration{
-		Debug:             *debug,
-		Addr:              *addr,
-		PromAddr:          *promAddr,
-		SessionCookieName: *sessionCookieName,
-		Provider:          *provider,
-		OIDCIssuerURL:     *oidcIssuerURL,
-		ClientID:          *clientId,
-		ClientSecret:      *clientSecret,
-		AuthPrefix:        *authPrefix,
-		SessionExpiration: *sessionExpiration,
 		StateConfiguration: state.Configuration{
-			CacheType: *cacheBackend,
 			Namespace: "github.com/clambin/traefik-simple-auth/state",
 			TTL:       10 * time.Minute,
-			MemcachedConfiguration: state.MemcachedConfiguration{
-				Addr: *cacheMemcachedAddr,
-			},
-			RedisConfiguration: state.RedisConfiguration{
-				Addr:     *cacheRedisAddr,
-				Database: *cacheRedisDatabase,
-				Username: *cacheRedisUsername,
-				Password: *cacheRedisPassword,
-			},
 		},
 	}
+	users := flag.String("users", "", "Comma-separated list of usernames to allow access")
+	flag.StringVar(&cfg.Addr, "addr", ":8080", "The address to listen on for HTTP requests")
+	flag.StringVar(&cfg.PromAddr, "prom", ":9090", "The address to listen on for Prometheus scrape requests")
+	flag.StringVar(&cfg.SessionCookieName, "session-cookie-name", "_traefik_simple_auth", "The cookie name to use for authentication")
+	flag.StringVar(&cfg.Provider, "provider", "google", "OAuth2 provider")
+	flag.StringVar(&cfg.OIDCIssuerURL, "provider-oidc-issuer", "https://accounts.google.com", "The OIDC Issuer URL to use (only used when provider is oidc")
+	flag.StringVar(&cfg.ClientID, "client-id", "", "OAuth2 Client ID")
+	flag.StringVar(&cfg.ClientSecret, "client-secret", "", "OAuth2 Client Secret")
+	flag.StringVar(&cfg.AuthPrefix, "auth-prefix", "auth", "Prefix to construct the authRedirect URL from the domain")
+	secret := flag.String("secret", "", "Secret to use for authentication (base64 encoded)")
+	domainString := flag.String("domain", "", "Domain to allow access")
+	flag.StringVar(&cfg.StateConfiguration.CacheType, "cache", "memory", "The backend to use for caching CSFR states. memory, memcached and redis are supported")
+	flag.StringVar(&cfg.StateConfiguration.MemcachedConfiguration.Addr, "cache-memcached-addr", "", "memcached address to use (only used when cache backend is memcached)")
+	flag.StringVar(&cfg.StateConfiguration.RedisConfiguration.Addr, "cache-redis-addr", "", "redis address to use (only used when cache backend is redis)")
+	flag.IntVar(&cfg.StateConfiguration.RedisConfiguration.Database, "cache-redis-database", 0, "redis database to use (only used when cache backend is redis)")
+	flag.StringVar(&cfg.StateConfiguration.RedisConfiguration.Username, "cache-redis-username", "", "Redis username (only used when cache backend is redis)")
+	flag.StringVar(&cfg.StateConfiguration.RedisConfiguration.Password, "cache-redis-password", "", "Redis password (only used when cache backend is redis)")
+	flag.DurationVar(&cfg.SessionExpiration, "expiry", 30*24*time.Hour, "How long a session remains valid")
+	flag.BoolVar(&cfg.Debug, "debug", false, "Log debug messages")
+	flag.Parse()
+
 	var err error
-	cfg.Domain, err = domain.New(*domainString)
-	if err != nil {
-		return Configuration{}, fmt.Errorf("invalid domain: %w", err)
-	}
-	cfg.Whitelist, err = whitelist.New(strings.Split(*users, ","))
-	if err != nil {
+	if cfg.Whitelist, err = whitelist.New(strings.Split(*users, ",")); err != nil {
 		return Configuration{}, fmt.Errorf("invalid whitelist: %w", err)
 	}
-	cfg.Secret, err = base64.StdEncoding.DecodeString(*secret)
-	if err != nil {
+	if cfg.Secret, err = base64.StdEncoding.DecodeString(*secret); err != nil {
 		return Configuration{}, fmt.Errorf("failed to decode secret: %w", err)
+	}
+	if cfg.Domain, err = domain.New(*domainString); err != nil {
+		return Configuration{}, fmt.Errorf("invalid domain: %w", err)
 	}
 	if cfg.ClientID == "" || cfg.ClientSecret == "" {
 		return Configuration{}, errors.New("must specify both client-id and client-secret")
