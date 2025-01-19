d804e62dd9b9b8024ec17416708e47b333f658cd
Christophe Lambin <christophe.lambin@gmail.com>
2025-01-19 20:44:09 +0100

    refactor: group authorization logic

--- traefik-simple-auth.go
+++ traefik-simple-auth.go
@@ -5,16 +5,21 @@ import (
 	"fmt"
 	"github.com/clambin/traefik-simple-auth/internal/cmd"
 	"github.com/prometheus/client_golang/prometheus"
+	"log/slog"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"runtime/debug"
 	"syscall"
 )
 
 var version = "change-me"
 
 func main() {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		slog.Info("successfully read build info", "version", info.GoVersion, "main", info.Main)
+	}
 	go func() { _ = http.ListenAndServe(":6000", nil) }()
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
